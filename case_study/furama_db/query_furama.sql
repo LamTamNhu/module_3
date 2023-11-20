b3b75fb0fd28e68cd45561dae0134b9a5f3b6502
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2023-11-20 17:18:21 +0700

    wip

--- case_study/furama_db/query_furama.sql
+++ case_study/furama_db/query_furama.sql
@@ -74,7 +74,12 @@ FROM
         LEFT JOIN
     loai_dich_vu AS ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
 GROUP BY dv.ma_dich_vu
-HAVING NOT hd.ngay_lam_hop_dong >= '2021-01-01';
+HAVING hd.ngay_lam_hop_dong NOT IN (SELECT 
+        ngay_lam_hop_dong
+    FROM
+        hop_dong
+    WHERE
+        ngay_lam_hop_dong >= '2021-01-01');
 
 -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
 SELECT 
@@ -128,4 +133,30 @@ select hd.ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, sum(hdct.
 from hop_dong as hd
 left join hop_dong_chi_tiet as hdct on hd.ma_hop_dong = hdct.ma_hop_dong
 left join dich_vu_di_kem as dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
-GROUP BY hd.ma_hop_dong
+GROUP BY hd.ma_hop_dong;
+
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+select dvdk.ma_dich_vu_di_kem,ten_dich_vu_di_kem,gia,don_vi,trang_thai
+from dich_vu_di_kem  as dvdk
+join hop_dong_chi_tiet as hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join hop_dong as hd on hd.ma_hop_dong=hdct.ma_hop_dong
+join khach_hang as kh on kh.ma_khach_hang= hd.ma_khach_hang
+join loai_khach as lk on kh.ma_loai_khach=lk.ma_loai_khach
+where lk.ten_loai_khach ='diamond' and (kh.dia_chi like '%vinh%' or kh.dia_chi like '%quảng ngãi%');
+
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+select hd.ma_hop_dong,nv.ho_ten,kh.ho_ten,kh.so_dien_thoai,dv.ten_dich_Vu,sum(hdct.so_luong) as so_luong_dich_vu_di_kem
+from hop_dong as hd
+left join nhan_vien as nv on nv.ma_nhan_vien = hd.ma_nhan_vien
+left join khach_hang as kh on kh.ma_khach_hang=hd.ma_khach_hang
+left join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu
+left join hop_dong_chi_tiet as hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+GROUP BY dv.ma_dich_vu,ngay_lam_hop_dong
+having hd.ngay_lam_hop_dong not in (
+select ngay_lam_hop_dong
+from hop_dong
+where ngay_lam_hop_dong BETWEEN '2021-01-01' and '2021-30-06')
+and ngay_lam_hop_dong BETWEEN '2020-09-01' and '2020-12-31';
+
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+
